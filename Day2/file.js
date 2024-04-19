abcccfcc39131840952264c020bd04a131c57e6b
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2024-04-19 12:14:26 +0530

    learning more methods

--- Day2/file.js
+++ Day2/file.js
@@ -31,4 +31,22 @@
 // })
 
 // Copy file
-fs.copyFileSync('./test1.txt', 'copied.txt')
+// fs.copyFileSync('./test1.txt', 'copied.txt')
+
+// append file
+// fs.appendFileSync("./test1.txt", "\n hello Rajeev")
+
+// appending inside the file
+// fs.appendFileSync("./test1.txt", new Date().getTime().toLocaleString())
+
+// Delete file
+// fs.unlinkSync('./text.txt')
+
+// for knowing the stats of the files
+// console.log(fs.statSync("./test1.txt"));
+
+// make new directory
+// fs.mkdirSync("new folder")
+
+// make new folder insider directory
+// fs.mkdirSync('./new folder/a')
